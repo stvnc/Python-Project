6ebe69938831de65018e3c6d00896a941ccc4d2a
stvnc <vincentangelo96@yahoo.com>
2019-11-08 11:36:21 +0700

    Bug Fixes if 2 parameter are the same

--- exam_practice/fpbAndKpk.py
+++ exam_practice/fpbAndKpk.py
@@ -14,13 +14,16 @@ def findFpbEuclidean(param, param2):
     return param
 
 def findKPK(param, param2):
+    greater = 1
     if param > param2:
         greater = param
     elif param < param2:
         greater = param2
+    else:
+        greater = param
     
     while(True):
-        if((greater%param==0) and (greater%param2==0)):
+        if(greater%param==0) and (greater%param2==0):
             kpk = greater
             break
         greater+=1
@@ -28,6 +31,6 @@ def findKPK(param, param2):
 
 userInput1 = int(input('Masukkan angka pertama: '))
 userInput2 = int(input('Masukkan angka kedua: '))
-print(findKPK(userInput1, userInput2))
-print(findFpb(userInput1, userInput2))
-print(findFpbEuclidean(userInput1, userInput2))
+print(f'KPK dari {userInput1} dan {userInput2} adalah {findKPK(userInput1, userInput2)}')
+print(f'FPB dari {userInput1} dan {userInput2} adalah {findFpb(userInput1, userInput2)}')
+print(f'FPB Euclidean dari {userInput1} dan {userInput2} adalah {findFpbEuclidean(userInput1, userInput2)}')
