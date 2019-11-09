ee85ff2c35430626ca6c30e6d59dfad2b3eb5536
stvnc <vincentangelo96@yahoo.com>
2019-11-09 15:46:00 +0700

    Full Version

--- practice/pertemuan.py
+++ practice/pertemuan.py
@@ -1,5 +1,15 @@
 distance = 120
 speedA = 60
 speedB = 40
+jamMulai = 9
+menit = "0"
 
-m
+pertemuanInJam = int(distance / (speedA+speedB))
+pertemuanInMenit = int((distance % (speedA+speedB))/100 * 60)
+if pertemuanInMenit < 10:
+    pertemuanInMenit = str(pertemuanInMenit)
+    pertemuanInMenit = menit + pertemuanInMenit    
+jamPertemuan = str(jamMulai + pertemuanInJam)+":"+str(pertemuanInMenit)
+
+print(f'Mereka akan bertemu dalam {pertemuanInJam} jam {pertemuanInMenit} menit')
+print(f'Mereka akan bertemu pada pukul {jamPertemuan}')
