b012322f9c592241d0819902b378e34bc67bf2d1
stvnc <vincentangelo96@yahoo.com>
2019-11-09 15:18:03 +0700

    Update Function

--- exam_practice/findSet.py
+++ exam_practice/findSet.py
@@ -28,10 +28,34 @@ def printFactor(param):
     for i in range(1, param+1):
         if param % i == 0:
             newList.append(i)
-    return newList            
+    return newList
 
+def recursiveMultiplication(param, param2):
+    temp = 1
+    for i in range(param2):
+        temp *= param
+    return temp
+
+def findFpb(param, param2):
+    dict1 = dict(param)
+    dict2 = dict(param2)
+    temp = 1
+
+    for i in list(dict1.keys()):
+        if i in dict2.keys():
+            if dict1[i] > dict2[i]:
+                temp *=  recursiveMultiplication(i, dict2[i])
+            else:
+                temp *= recursiveMultiplication(i, dict1[i])
+    return temp
+            
 userInput = int(input('Masukkan angka yang ingin dicek: '))
+userInput2 = int(input('Masukkan angka yang ingin dicek: '))
 setList = findSet(userInput)
+newSetList = findSet(userInput2)
+setPower = findPower(userInput, setList)
+setPower2 = findPower(userInput2, newSetList)
+print(findFpb(setPower, setPower2))
 print(setList)
 print(findPower(userInput, setList))
 print(printFactor(userInput))
