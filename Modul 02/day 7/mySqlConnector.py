75e9271aa319bad2cec6d699bcce737730e3e7c2
stvnc <vincentangelo96@yahoo.com>
2019-11-22 09:14:17 +0700

    Updated Code

---
+++ Modul 02/day 7/mySqlConnector.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    port =  3306,
+    user = 'root',
+    passwd = 'vincent28',
+    auth_plugin = 'mysql_native_password',
+    database = 'day7'
+)
+print(db)
+
+c = db.cursor()
+sql = 'insert into karyawan(nama, gaji) values (%s, %s)'
+val = ('Andi', 15000000)
+c.execute(sql, val)
+db.commit()
+# print(c.fetchall())
+print(c.rowcount, 'Data tersimpan')
